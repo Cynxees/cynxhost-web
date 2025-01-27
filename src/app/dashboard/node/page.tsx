7e1490f71df509146f7b1257c8422ba0fe5ffeb6
Derwin Oesmanto <oesmantoderwin@gmail.com>
2025-01-27 17:58:46 +0700

    fix build issue

--- src/app/dashboard/node/page.tsx
+++ src/app/dashboard/node/page.tsx
@@ -1,10 +1,15 @@
 import { GetPersistentNode } from "@/app/_lib/services/persistentNodeService";
 import DashboardNodeContent from "./content";
+import { Suspense } from "react";
 
 interface SearchParams {
   id?: string;
 }
 
 export default async function DashboardNodeWrapper() {
-  return <DashboardNodeContent />;
+  return (
+    <Suspense>
+      <DashboardNodeContent />;
+    </Suspense>
+  );
 }
